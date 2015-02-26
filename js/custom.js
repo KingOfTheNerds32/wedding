1796894ae43ce727a145015d18f232cb36b487fd
KingOfTheNerds32 <mlarner@alumni.usc.edu>
2015-02-25 22:22:48 -0800

    Instagram feed. Broken gallery

--- js/custom.js
+++ js/custom.js
@@ -3,39 +3,39 @@ var main = function(){
     //UTC is 8 hours different
     var start = new Date(Date.UTC(2014, 10, 24, 1))
     console.log(start);
-        start = start.getTime() / 1000;
-        var date = new Date(Date.UTC(2015, 7, 30, 1, 30, 0));
-        console.log(date);
-   var now   = new Date();
-   var diff = date.getTime() / 1000 - now.getTime() / 1000;
-   now = now.getTime() / 1000;
-   date = date.getTime() / 1000;
+    start = start.getTime() / 1000;
+    var date = new Date(Date.UTC(2015, 7, 30, 1, 30, 0));
+    console.log(date);
+    var now   = new Date();
+    var diff = date.getTime() / 1000 - now.getTime() / 1000;
+    now = now.getTime() / 1000;
+    date = date.getTime() / 1000;
 
   // var clock = $('.clock').FlipClock(diff, {
   //  clockFace: 'DailyCounter',
   //  countdown: true
   //});
 
-  if ($('.countdown').length){
-   $('.countdown').final_countdown({
-       'start': 1362139200,
-       'end': date,
+if ($('.countdown').length){
+ $('.countdown').final_countdown({
+   'start': 1362139200,
+   'end': date,
 
-       'now': now
-   });
- }
+   'now': now
+ });
+}
 
-   document.getElementById('links').onclick = function (event) {
-    event = event || window.event;
-    var target = event.target || event.srcElement,
-    link = target.src ? target.parentNode : target,
-    options = {
-      index: link, 
-      event: event
-    },
-    links = this.getElementsByTagName('a');
-    blueimp.Gallery(links, options);
-  };
+document.getElementById('links').onclick = function (event) {
+  event = event || window.event;
+  var target = event.target || event.srcElement,
+  link = target.src ? target.parentNode : target,
+  options = {
+    index: link, 
+    event: event
+  },
+  links = this.getElementsByTagName('a');
+  blueimp.Gallery(links, options);
+};
 
 //$( "#story" ).load( "timeline_new.html");
 
@@ -49,31 +49,31 @@ var main = function(){
 
   var info_counter = 0;
   $(".infographic").each(function () {
-      $this = $(this);
-      var entry = 0;
-      switch (info_counter % 3) {
-          case 0:
-              entry = x_pos;
-              break;
-          case 1:
-              entry = x_pos * -1;
-              break;
-          default:
-              entry = 0;
-      };
+    $this = $(this);
+    var entry = 0;
+    switch (info_counter % 3) {
+      case 0:
+      entry = x_pos;
+      break;
+      case 1:
+      entry = x_pos * -1;
+      break;
+      default:
+      entry = 0;
+    };
       // console.log(entry);
       if (entry == 0) {
-          controller.addTween($this, TweenMax.from( $this, 2, {css:{opacity: 0, scaleX: .5}, ease: Elastic.easeOut.config(1, 0.75), y: 0 }), scrollDuration);
+        controller.addTween($this, TweenMax.from( $this, 2, {css:{opacity: 0, scaleX: .5}, ease: Elastic.easeOut.config(1, 0.75), y: 0 }), scrollDuration);
       }
       else {
-          controller.addTween($this, TweenMax.from( $this, 1, { css: { left: entry }, ease: Elastic.easeOut.config(1, 0.75), y: 0 }), scrollDuration);
+        controller.addTween($this, TweenMax.from( $this, 1, { css: { left: entry }, ease: Elastic.easeOut.config(1, 0.75), y: 0 }), scrollDuration);
       }
       info_counter++;
-  });
+    });
 
   $(".section-title").each(function(){
     $this = $(this);
-      controller.addTween($this, TweenMax.from( $this, 2, {css:{opacity: 0, scaleX: .5}, ease: Elastic.easeOut.config(1, 0.75), y: 0 }), scrollDuration);
+    controller.addTween($this, TweenMax.from( $this, 2, {css:{opacity: 0, scaleX: .5}, ease: Elastic.easeOut.config(1, 0.75), y: 0 }), scrollDuration);
   })
 
   // controller.addTween("#proposal", TweenMax.from( $("#proposal"), .5, {css: {opacity: 0}}), scrollDuration);
@@ -92,7 +92,37 @@ var main = function(){
   // controller.addTween('#scale-it', TweenMax.fromTo( $('#scale-it'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}), scrollDuration);
   // controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), scrollDuration); // 100 px offset for better timing
   resizer();
-}
+
+    //   var feed = new Instafeed({
+    //     get: 'tagged',
+    //     tagName: 'awesome',
+    //     clientId: 'a415febb075e4f78a8db95973981df84',
+    //     template: '<div class="col-md-4 feed_card">{{caption}}<a class="animation" href={{link}}"><img class="center-block img-responsive"src="{{image}}" /></a></div>',
+    //     limit: 3
+    // });
+    // feed.run();
+    jQuery("#gallery").camera();
+
+    var feed = new Instafeed({
+  clientId: 'a415febb075e4f78a8db95973981df84',
+  limit: 20,
+  sortBy: 'most-liked',
+  after: function () {
+    var images = $("#instafeed").find('a');
+    $.each(images, function(index, image) {
+      var delay = (index * 75) + 'ms';
+      $(image).css('-webkit-animation-delay', delay);
+      $(image).css('-moz-animation-delay', delay);
+      $(image).css('-ms-animation-delay', delay);
+      $(image).css('-o-animation-delay', delay);
+      $(image).css('animation-delay', delay);
+      $(image).addClass('animated flipInX');
+    });
+  },
+  template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
+});
+feed.run();
+
 };
 
 var resizer = function(){
